c79b018cb95639982118b71bc9a82cb16f82b7cf
Rupam Karmakar <73836166+Rupam-07@users.noreply.github.com>
2023-01-21 20:08:42 +0530

    21st jan Solution

---
+++ 21-01_Minimum X (xor) A.java
@@ -0,0 +1,52 @@
+class Solution {
+    public static int minVal(int a, int b) {
+        // code here
+        int x=Integer.bitCount(a);
+
+        int y=Integer.bitCount(b);
+
+        int min=Integer.MAX_VALUE;
+
+        if(x==y)
+
+        return a;
+
+        else if(x>y)
+
+        {
+
+            for(int i=a;i>0;i--)
+
+            {
+
+                min=Math.min(min,i^a);
+
+                if(Integer.bitCount(i)==y)
+
+                return i;
+
+            }
+
+        }
+
+        else if(y>x)
+
+        {
+
+            for(int i=a;i<Integer.MAX_VALUE;i++)
+
+            {
+
+               min=Math.min(min,i^b);
+
+                if(Integer.bitCount(i)==y)
+
+                return i; 
+
+            }
+
+        }
+
+        return 0;
+    }
+}
