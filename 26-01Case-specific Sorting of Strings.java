4541a3b413e787b3f4294f3d5c048a47d8531c82
Rupam Karmakar <73836166+Rupam-07@users.noreply.github.com>
2023-01-26 11:13:04 +0530

    26th jan solution

---
+++ 26-01Case-specific Sorting of Strings.java
@@ -0,0 +1,105 @@
+class Solution
+
+{
+
+    public static String caseSort(String str)
+
+    {
+
+        // Your code here
+
+        if(str.length()==1) return str;
+
+        int[] cap = new int[26];
+
+        int[] sma = new int[26];
+
+        Arrays.fill(cap,0);
+
+        Arrays.fill(cap,0);
+
+        int z,y;
+
+        for(int i=0;i<str.length();i++){
+
+            z = (int)(str.charAt(i));
+
+            if(z>=65 && z<=90){
+
+                y = z-65;
+
+                cap[y]++;
+
+            }
+
+            else{
+
+                y = z-97;
+
+                sma[y]++;
+
+            }
+
+        }
+
+        ArrayList<Integer> capIndA = new ArrayList<>();
+
+        ArrayList<Integer> smaIndA = new ArrayList<>();
+
+        for(int i=0;i<26;i++){
+
+            while(cap[i]-->0){
+
+                capIndA.add(i);
+
+            }
+
+        }
+
+        for(int i=0;i<26;i++){
+
+            while(sma[i]-->0){
+
+                smaIndA.add(i);
+
+            }
+
+        }
+
+        StringBuffer t = new StringBuffer();
+
+        char ch;
+
+        int k=-1,j=-1;
+
+        for(int i=0;i<str.length();i++){
+
+            z = (int)(str.charAt(i));
+
+            if(z>=65 && z<=90){
+
+                k++;
+
+                ch = (char)(capIndA.get(k)+65);
+
+                t.append(ch);
+
+            }
+
+            else{
+
+                j++;
+
+                ch = (char)(smaIndA.get(j)+97);
+
+                t.append(ch);
+
+            }
+
+        }
+
+        return t.toString();
+
+    }
+
+}
